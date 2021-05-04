5a025f5eae3742274ecfb780576ee37bed5793c6
RobertElias <robertelias@pop-os.localdomain>
2021-05-04 11:45:44 -0700

    mockdataschema built

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function (req, res){
 
 })
 
+app.get('/users', function (req, res){
+  //Hard coding data here
+  res.json([
+    {"id": 1,"firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy", "lastName":"Norton", "email":"tnorton@gmail.com"},
+    {"id": 3,"firstName":"Tina", "lastName":"Lee", "email":"lee.tina@gmail.com"}
+  ]);
+
+})
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
